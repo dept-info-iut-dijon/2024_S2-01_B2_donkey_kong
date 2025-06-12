462d380eee057780d711b2465c59c459a4cf7cc0
MarwanHimeur <marwan.himeur@iut-dijon.u-bourgogne.fr>
2025-06-12 17:48:39 +0200

    tache 2.6 creation d'une classe parametres pour les gerer dans la fenetre

---
+++ Donkey_Kong_Metier/Parametres.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donkey_Kong_Metier
+{
+    public class Parameters
+    {
+        public Langues Langue { get; set; } = Langues.Français;
+        public double Volume { get; set; } = 0.5;
+        public bool EstMuet { get; set; } = false;
+    }
+}
