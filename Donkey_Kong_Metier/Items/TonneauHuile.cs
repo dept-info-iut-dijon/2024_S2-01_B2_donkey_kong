8d5657b54aea26697136cca6d95064fe4d67d71d
ELALLAM <mehdi.el-allam@iut-dijon.u-bourgogne.fr>
2025-06-12 11:32:56 +0200

    2.2 items de base

---
+++ Donkey_Kong_Metier/Items/TonneauHuile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donkey_Kong_Metier.Items
+{
+    internal class TonneauHuile
+    {
+    }
+}
