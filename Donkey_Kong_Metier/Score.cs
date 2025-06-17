95147c6c87032687133707afe1d20e0e2e0bdcd4
RomainSouveton <romain.souveton@iut-dijon.u-bourgogne.fr>
2025-06-17 17:05:58 +0200

    ajout de la classe score

---
+++ Donkey_Kong_Metier/Score.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donkey_Kong_Metier
+{
+    public class Score
+    {
+        #region --attributs--
+        //score obtenu par le joueur
+        private int scoreFinal;
+        #endregion
+
+        #region --Propriété--
+        public int ScoreFinal { get; }
+        #endregion
+
+        #region --Constructeur
+        /// <summary>
+        /// Constructeur pour initialiser le score du joueur
+        /// </summary>
+        public Score()
+        {
+            ScoreFinal = 0;
+        }
+        #endregion
+
+        #region --Méthodes--    
+        /// <summary>
+        /// Méthode qui sert à jouter un score au joueur en fonction de ses actions
+        /// </summary>
+        /// <param name="score"></param>
+        public void AjouterScore(int score)
+        {
+            scoreFinal += score;
+        }
+
+        /// <summary>
+        /// Représentation textuelle du score
+        /// </summary>
+        /// <returns>Chaîne formatée</returns>
+        public override string ToString()
+        {
+            return ScoreFinal + " points";
+        }
+        #endregion
+    }
+}
