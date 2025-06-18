f80ed04c29e37df9cd1a30210aa3dc2fc3fd9be7
ELALLAM <mehdi.el-allam@iut-dijon.u-bourgogne.fr>
2025-06-18 23:33:51 +0200

    teste unitaire des colisions diviser en 3 permiere les colision avec le mario deuxieme colision de la boule de feu et la derniere les colisions du barils

---
+++ TestColision/TbouledeFeu.cs
@@ -0,0 +1,63 @@
+﻿using Donkey_Kong_Metier;
+using Donkey_Kong_Metier.Items;
+using TestsMouvement;
+
+
+namespace TestColision
+{
+    /// <summary>
+    /// Test unitaires pour CollideEffect de BouleFeu
+    /// </summary>
+    [CollectionDefinition("NonParallelCollection", DisableParallelization = true)]
+    public class TCollisionBouleFeu
+    {
+        /// <summary>
+        /// Test: BouleFeu collision avec Echelle
+        /// </summary>
+        [Fact]
+        public void BouleFeu_echelle()
+        {
+            
+            FakeScreen screen = new FakeScreen();
+            LeJeu jeu = new LeJeu(screen, "", "");
+
+            BouleFeu bouleFeu = new BouleFeu(100, 100, jeu);
+            Echelle echelle = new Echelle(150, 150, jeu);
+
+
+            try
+            {
+                bouleFeu.CollideEffect(echelle);
+            }
+            catch (Exception )
+            {
+                Assert.True(false);
+            }
+        }
+
+        /// <summary>
+        /// boule de feu en colision avec un objert pas gere 
+        /// </summary>
+        [Fact]
+        public void BouledeFeu_objet_nongerer()
+        {
+           
+            FakeScreen screen = new FakeScreen();
+            LeJeu jeu = new LeJeu(screen, "", "");
+
+            BouleFeu bouleFeu = new BouleFeu(100, 100, jeu);
+            Plateforme plateforme = new Plateforme(150, 150, jeu);
+
+
+            try
+            {
+                bouleFeu.CollideEffect(plateforme);
+            }
+            catch (Exception)
+            {
+                Assert.True(false);
+            }
+
+        }
+    }
+    }
